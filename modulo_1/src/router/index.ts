92c9efa2bd3cb6bc738245192c50d83740999709
Andre Sassi <andre.sassi07@gmail.com>
2026-01-30 01:14:32 -0300

    fix: redireciona para home se o access token estiver inválido ou ausente

--- modulo_1/src/router/index.ts
+++ modulo_1/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Callback from '../views/Callback.vue'
 import Landingpage from '../views/LandingPage.vue'
 import Produtos from '../views/Produtos.vue'
+import ProdutoForm from '../components/ProdutoForm.vue'
 
 const routes = [
   {
@@ -18,7 +19,18 @@ const routes = [
     path: '/produtos',
     name: 'produtos',
     component: Produtos
-  }
+  },
+  {
+    path: '/produtos/novo',
+    name: 'produtonovo',
+    component: ProdutoForm
+  },
+  {
+    path: '/produtos/editar/:id',
+    name: 'produtoeditar',
+    component: ProdutoForm,
+    props: true
+  },
 ]
 
 const router = createRouter({
@@ -26,6 +38,15 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach((to, _, next) => {
+  const token = localStorage.getItem('bling_access_token');
+  const rotasPublicas = ['home', 'callback'];
 
+  if (!rotasPublicas.includes(to.name as string) && !token) {
+    next({ name: 'home' });
+  } else {
+    next();
+  }
+});
 
 export default router
