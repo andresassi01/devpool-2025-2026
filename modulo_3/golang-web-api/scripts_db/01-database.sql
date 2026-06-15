74413a5d73420b83ec3e8d2dd577e0898aa59b43
Andre Sassi <andre.sassi07@gmail.com>
2026-06-14 23:11:58 -0300

    chore: adicionar script de migracao de banco de dados para clientes

--- modulo_3/golang-web-api/scripts_db/01-database.sql
+++ modulo_3/golang-web-api/scripts_db/01-database.sql
@@ -6,3 +6,26 @@ CREATE TABLE IF NOT EXISTS tasks (
     created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+USE `devpool_erp`;
+
+-- Cria a tabela nova de clientes separada
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `nome` VARCHAR(512) NOT NULL 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Modifica a tabela de vendas antiga 
+-- Adiciona a nova coluna de vínculo (idCliente)
+ALTER TABLE `vendas` 
+ADD COLUMN `idCliente` INT NOT NULL AFTER `id`;
+
+-- Cria a restrição de Chave Estrangeira (FK) para garantir a integridade
+ALTER TABLE `vendas`
+ADD CONSTRAINT `fk_vendas_cliente`
+FOREIGN KEY (`idCliente`) REFERENCES `clientes`(`id`)
+ON DELETE RESTRICT;
+
+-- Remove o campo 'nomeCliente'
+ALTER TABLE `vendas` 
+DROP COLUMN `nomeCliente`;
